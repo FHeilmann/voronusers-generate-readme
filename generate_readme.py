581b08d279c7de1329c72f4b15791693484b7e19
Florian Heilmann <Florian.Heilmann@gmx.net>
2023-06-28 22:38:57 +0200

    Create action V1

---
+++ generate_readme.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import yaml
+import textwrap
+
+preamble = """
+# Mods
+
+Printer mods for Voron 3D printers
+
+## Legacy printers
+
+Mods for legacy printers can be found [here](../legacy_printers/printer_mods).
+If one of your legacy mods applies to a current Voron 3D printer and therefore should be included in this list, 
+contact the admins on Discord to have your mod moved to this folder.
+
+---
+
+| Creator | Mod title | Description | Printer compatibility |
+| --- | --- | --- | --- |
+"""
+
+def main():
+  yaml_list = Path(".").glob("**/.metadata.yml")
+  final_readme = preamble
+  prev_username = ""
+  for yml in yaml_list:
+      with open(yml, 'r') as f:
+          content = yaml.safe_load(f)
+          title = textwrap.shorten(content["title"], width=35, placeholder="...")
+          creator = yml.parts[0] if yml.parts[0] != prev_username else ""
+          description = textwrap.shorten(content["description"], width=70, placeholder="...")
+          final_readme += (f'| {creator} | [{title}]({yml.relative_to(".").parent}) | {description} | {", ".join(sorted(content["printer_compatibility"]))} |\n')
+          prev_username = yml.parts[0]
+  print(final_readme)
+  with open("README.md", 'w') as f:
+    f.write(final_readme)
+
+if __name__ == "__main__":
+  main()
